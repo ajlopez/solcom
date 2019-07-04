43845c785f00f5fdff202ef8f37407a24a2990c6
ajlopez <webmaster@ajlopez.com>
2019-07-04 14:53:16 -0300

    Using geast.toObject in AST sample

--- samples/ast/ast.js
+++ samples/ast/ast.js
@@ -1,5 +1,6 @@
 
 const parser = require('../../lib/parser');
+const geast = require('geast');
 const fs = require('fs');
 
 const code = fs.readFileSync(process.argv[2]).toString();
@@ -8,45 +9,4 @@ console.log(code);
 
 const result = parser.parse('program', code);
 
-console.log(JSON.stringify(toObject(result), null, 2));
-
-function arrayToObject(array) {
-    const result = [];
-    
-    if (array.length === 0)
-        return result;
-    
-    for (let k = 0; k < array.length; k++)
-        result.push(toObject(array[k]));
-    
-    return result;
-}
-
-function toObject(node) {
-    if (node == null)
-        return null;
-    
-    if (Array.isArray(node))
-        return arrayToObject(node);
-    
-    const result = {};
-    
-    for (let n in node) {
-        if (n === 'process' || n === 'define')
-            continue;
-        
-        let value;
-        
-        if (typeof node[n] === 'function')
-            value = node[n]();
-        else
-            value = node[n];
-        
-        if (value && typeof value === 'object')
-            result[n] = toObject(value);
-        else
-            result[n] = value;
-    }
-    
-    return result;
-}
+console.log(JSON.stringify(geast.toObject(result), null, 2));
