2d6979cf2b7b11cd3d9077b5071dbab5a7376fae
ajlopez <webmaster@ajlopez.com>
2020-05-11 11:41:14 -0300

    Using geast.toObject in term tests

--- test/terms.js
+++ test/terms.js
@@ -1,64 +1,74 @@
 
 const parser = require('../lib/parser');
+const geast = require('geast');
 
 exports['parse lowercase name'] = function (test) {
     const result = parser.parse('name', 'foo');
     
-    match(test, result, { ntype: 'name', name: 'foo' });
+    test.deepEqual(geast.toObject(result), {
+        ntype: 'name', name: 'foo' });
 };
 
 exports['parse name with digits'] = function (test) {
     const result = parser.parse('name', 'foo42');
     
-    match(test, result, { ntype: 'name', name: 'foo42' });
+    test.deepEqual(geast.toObject(result), {
+        ntype: 'name', name: 'foo42' });
 };
 
 exports['parse name with underscore and digits'] = function (test) {
     const result = parser.parse('name', 'foo_42');
     
-    match(test, result, { ntype: 'name', name: 'foo_42' });
+    test.deepEqual(geast.toObject(result), {
+        ntype: 'name', name: 'foo_42' });
 };
 
 exports['parse mixed case name'] = function (test) {
     const result = parser.parse('name', 'Foo');
     
-    match(test, result, { ntype: 'name', name: 'Foo' });
+    test.deepEqual(geast.toObject(result), {
+        ntype: 'name', name: 'Foo' });
 };
 
 exports['parse uppercase name'] = function (test) {
     const result = parser.parse('name', 'BAR');
     
-    match(test, result, { ntype: 'name', name: 'BAR' });
+    test.deepEqual(geast.toObject(result), {
+        ntype: 'name', name: 'BAR' });
 };
 
 exports['parse name with underscore'] = function (test) {
     const result = parser.parse('name', 'foo_bar');
     
-    match(test, result, { ntype: 'name', name: 'foo_bar' });
+    test.deepEqual(geast.toObject(result), {
+        ntype: 'name', name: 'foo_bar' });
 };
 
 exports['parse name with initial underscore'] = function (test) {
     const result = parser.parse('name', '_foo');
     
-    match(test, result, { ntype: 'name', name: '_foo' });
+    test.deepEqual(geast.toObject(result), {
+        ntype: 'name', name: '_foo' });
 };
 
 exports['parse integer'] = function (test) {
     const result = parser.parse('integer', '42');
     
-    match(test, result, { ntype: 'constant', value: 42 });
+    test.deepEqual(geast.toObject(result), {
+        ntype: 'constant', value: 42 });
 };
 
 exports['parse string'] = function (test) {
     const result = parser.parse('string', '"foo"');
     
-    match(test, result, { ntype: 'constant', value: 'foo' });
+    test.deepEqual(geast.toObject(result), {
+        ntype: 'constant', value: 'foo' });
 };
 
 exports['parse indexed term'] = function (test) {
     const result = parser.parse('term', 'a[10]');
     
-    match(test, result, {
+    test.deepEqual(geast.toObject(result), {
         ntype: 'indexed',
         target: {
             ntype: 'name',
@@ -70,21 +80,4 @@ exports['parse indexed term'] = function (test) {
         }
     });
 };
-
-function match(test, node, obj) {
-    test.ok(node);
-    
-    for (let n in obj) {
-        test.ok(node[n]);
-        test.equal(typeof node[n], 'function');
-        
-        const value = node[n]();
-        const expected = obj[n];
-        
-        if (typeof value === 'object')
-            match(test, value, expected);
-        else
-            test.strictEqual(value, expected);
-    }
-}
 
