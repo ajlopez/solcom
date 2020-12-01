6fb3b4f2227d18857b5a8189ac03d7d0caeb8c92
ajlopez <webmaster@ajlopez.com>
2020-12-01 13:49:30 -0300

    Parse variable storage

--- test/args.js
+++ test/args.js
@@ -1,12 +1,24 @@
 
 const parser = require('../lib/parser');
+const geast = require('geast');
 
 exports['parse argument'] = function (test) {
     const result = parser.parse('argument', 'uint value');
 
-    match(test, result, {
+    test.deepEqual(geast.toObject(result), {
         ntype: 'argument',
         name: 'value',
+        type: 'uint'
+    });
+};
+
+exports['parse argument with memory storage'] = function (test) {
+    const result = parser.parse('argument', 'uint memory value');
+
+    test.deepEqual(geast.toObject(result), {
+        ntype: 'argument',
+        name: 'value',
+        storage: 'memory',
         type: 'uint'
     });
 };
@@ -16,26 +28,4 @@ exports['parse empty argument list'] = function (test) {
 
     test.deepEqual(result, []);
 };
-
-function match(test, node, obj) {
-    test.ok(node);
-    
-    for (var n in obj) {
-        test.ok(node[n]);
-        
-        let value;
-        
-        if (typeof node[n] === 'function')
-            value = node[n]();
-        else
-            value = node[n];
-        
-        const expected = obj[n];
-        
-        if (value != null && typeof value === 'object')
-            match(test, value, expected);
-        else
-            test.strictEqual(value, expected);
-    }
-}
 
