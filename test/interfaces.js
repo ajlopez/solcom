23cf74c07a667036ac78f350e13230b88437234a
ajlopez <webmaster@ajlopez.com>
2020-12-16 08:06:47 -0300

    Parse interface with external function definition

--- test/interfaces.js
+++ test/interfaces.js
@@ -15,3 +15,26 @@ exports['parse empty interface'] = function (test) {
     });
 };
 
+exports['parse interface with function'] = function (test) {
+    const result = parser.parse('interface', 'interface Counter { function increment() external; }');
+    
+    test.deepEqual(geast.toObject(result), {
+        ntype: 'interface',
+        name: 'Counter',
+        body: {
+            ntype: 'sequence',
+            nodes: [
+                {
+                    ntype: 'method',
+                    name: 'increment',
+                    arguments: [],
+                    attributes: {
+                        visibility: 'external'
+                    },
+                }
+            ]
+        }
+    });
+};
+
+    
