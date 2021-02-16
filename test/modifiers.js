bdb9b8d3fe1e0e70d7fb0296050d51b2098c3790
ajlopez <webmaster@ajlopez.com>
2021-02-16 16:28:59 -0300

    Parse modifier with body

--- test/modifiers.js
+++ test/modifiers.js
@@ -35,3 +35,62 @@ exports['parse modifier with one argument'] = function (test) {
         }
     });
 };
+
+exports['parse modifier with two arguments'] = function (test) {
+    const result = parser.parse('modifier', 'modifier onlyOwner(address user, uint balance) {}');
+    
+    test.deepEqual(geast.toObject(result), {
+        ntype: 'modifier',
+        name: 'onlyOwner',
+        arguments: [
+            {
+                ntype: 'argument',
+                name: 'user',
+                type: 'address'
+            },
+            {
+                ntype: 'argument',
+                name: 'balance',
+                type: 'uint'
+            }
+        ],
+        body: {
+            ntype: 'sequence',
+            nodes: []
+        }
+    });
+};
+
+exports['parse modifier with body'] = function (test) {
+    const result = parser.parse('modifier', 'modifier onAction() { counter = counter + 1; }');
+    
+    test.deepEqual(geast.toObject(result), {
+        ntype: 'modifier',
+        name: 'onAction',
+        arguments: [],
+        body: {
+            ntype: 'sequence',
+            nodes: [
+                {
+                    expression: {
+                        left: {
+                            name: 'counter',
+                            ntype: 'name'
+                        },
+                        ntype: 'binary',
+                        operator: '+',
+                        right: {
+                            ntype: 'constant',
+                            value: 1
+                        }
+                    },
+                    lefthand: {
+                        name: 'counter',
+                        ntype: 'name'
+                    },
+                    ntype: 'assign'
+                }
+            ]
+        }
+    });
+};
