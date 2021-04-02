d62ca5a2723102f1d0b32e10342ea8a9915c403f
ajlopez <webmaster@ajlopez.com>
2021-04-02 10:20:37 -0300

    Parse struct with two members

--- test/structs.js
+++ test/structs.js
@@ -17,3 +17,24 @@ exports['parse struct with one member'] = function (test) {
         ]
     });
 };
+
+exports['parse struct with two members'] = function (test) {
+    const result = parser.parse('struct', 'struct Customer { string name; uint256 balance; }');
+    
+    test.deepEqual(geast.toObject(result), {
+        ntype: 'struct',
+        name: 'Customer',
+        members: [
+            {
+                ntype: 'member',
+                type: 'string',
+                name: 'name'
+            },
+            {
+                ntype: 'member',
+                type: 'uint256',
+                name: 'balance'
+            }
+        ]
+    });
+};
